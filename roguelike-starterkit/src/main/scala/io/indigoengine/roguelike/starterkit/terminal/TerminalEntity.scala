3f46a5c46b953304715e81c80542d3589b198c58
Dave Smith <davesmith.00000@gmail.com>
2023-11-06 20:18:23 +0000

    Refactor MapTile into it's own file

--- roguelike-starterkit/src/main/scala/io/indigoengine/roguelike/starterkit/terminal/TerminalEntity.scala
+++ roguelike-starterkit/src/main/scala/io/indigoengine/roguelike/starterkit/terminal/TerminalEntity.scala
@@ -229,20 +229,3 @@ object TerminalEntity:
           }
 
       }
-
-final case class MapTile(char: Tile, foreground: RGBA, background: RGBA):
-  def withChar(newChar: Tile): MapTile =
-    this.copy(char = newChar)
-
-  def withForegroundColor(newColor: RGBA): MapTile =
-    this.copy(foreground = newColor)
-
-  def withBackgroundColor(newColor: RGBA): MapTile =
-    this.copy(background = newColor)
-
-object MapTile:
-  def apply(char: Tile): MapTile =
-    MapTile(char, RGBA.White, RGBA.Zero)
-
-  def apply(char: Tile, foreground: RGBA): MapTile =
-    MapTile(char, foreground, RGBA.Zero)

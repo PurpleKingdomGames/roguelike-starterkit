49b0de872e5c58917dc1aea7c56304e5b2e3e87f
Dave Smith <davesmith.00000@gmail.com>
2023-11-06 20:21:50 +0000

    Refactor TerminalClones into it's own file

---
+++ roguelike-starterkit/src/main/scala/io/indigoengine/roguelike/starterkit/terminal/TerminalClones.scala
@@ -0,0 +1,15 @@
+package io.indigoengine.roguelike.starterkit.terminal
+
+import indigo.*
+import io.indigoengine.roguelike.starterkit.Tile
+import scala.annotation.tailrec
+
+final case class TerminalClones(blanks: Batch[CloneBlank], clones: Batch[CloneTiles]):
+  def |+|(other: TerminalClones): TerminalClones =
+    combine(other)
+  def combine(other: TerminalClones): TerminalClones =
+    TerminalClones(blanks ++ other.blanks, clones ++ other.clones)
+
+object TerminalClones:
+  def empty: TerminalClones =
+    TerminalClones(Batch.empty, Batch.empty)

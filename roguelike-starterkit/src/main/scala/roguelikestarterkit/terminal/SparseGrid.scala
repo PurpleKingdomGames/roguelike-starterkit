6670c900e42a1ef6d5f3442147ec06669d39920f
Dave Smith <davesmith.00000@gmail.com>
2024-01-01 23:00:22 +0000

    SpareGrid Scaladocs

--- roguelike-starterkit/src/main/scala/roguelikestarterkit/terminal/SparseGrid.scala
+++ roguelike-starterkit/src/main/scala/roguelikestarterkit/terminal/SparseGrid.scala
@@ -8,15 +8,17 @@ import scala.annotation.tailrec
 import scalajs.js
 import js.JSConverters._
 
-/** */
+/** SparseGrid is a collection for managing grid of values where grid squares may or may not
+  * actually contain a value.
+  */
 final class SparseGrid[A](
     val size: Size,
     values: Batch[js.UndefOr[A]]
 ):
-  /** */
+  /** Returns the length of the grid, i.e. width * height */
   lazy val length: Int = size.width * size.height
 
-  /** */
+  /** Put a value into the grid */
   def put(
       coords: Point,
       value: A
@@ -26,7 +28,7 @@ final class SparseGrid[A](
       values(SparseGrid.pointToIndex(coords, size.width)) = value
     )
 
-  /** */
+  /** Put a batch of values into the grid */
   def put(newValues: Batch[(Point, A)]): SparseGrid[A] =
     val arr = values.toJSArray
     newValues.foreach { t =>
@@ -38,24 +40,24 @@ final class SparseGrid[A](
 
     new SparseGrid(size, Batch(arr))
 
-  /** */
+  /** Put a batch of values into the grid, at an offset position */
   def put(values: Batch[(Point, A)], offset: Point): SparseGrid[A] =
     put(values.map(p => p._1 + offset -> p._2))
 
-  /** */
+  /** Put repeating values into the grid */
   def put(values: (Point, A)*): SparseGrid[A] =
     put(Batch.fromSeq(values))
 
-  /** */
+  /** Fill the grid with the given value */
   def fill(value: A): SparseGrid[A] =
     new SparseGrid[A](size, Batch.fill(size.width * size.height)(value))
 
-  /** */
+  /** Get the value at the given coordinates */
   def get(coords: Point): Option[A] =
     val idx = SparseGrid.pointToIndex(coords, size.width)
     values(idx).toOption
 
-  /** */
+  /** Remove the value at the given coordinates */
   def remove(coords: Point): SparseGrid[A] =
     new SparseGrid(
       size,
@@ -142,19 +144,20 @@ final class SparseGrid[A](
 
     Batch(acc.collect { case p if p.isDefined => p.get })
 
-  /** */
+  /** Alias for `combine`. Takes all of the 'set' entries of 'other' and puts them into this grid.
+    */
   def |+|(other: SparseGrid[A]): SparseGrid[A] =
     combine(other)
 
-  /** */
+  /** Takes all of the 'set' entries of 'other' and puts them into this grid. */
   def combine(other: SparseGrid[A]): SparseGrid[A] =
     put(other.toPositionedBatch)
 
-  /** */
+  /** Combines two grids be insetting one within the other, at an offset. */
   def inset(other: SparseGrid[A], offset: Point): SparseGrid[A] =
     put(other.toPositionedBatch, offset)
 
-  /** */
+  /** Modify a specific point on the grid */
   def modifyAt(coords: Point)(modifier: Option[A] => Option[A]): SparseGrid[A] =
     val idx = SparseGrid.pointToIndex(coords, size.width)
     val t   = values(idx)
@@ -163,7 +166,7 @@ final class SparseGrid[A](
       case None        => remove(coords)
       case Some(value) => put(coords, value)
 
-  /** */
+  /** Map over and modify all the points on the grid */
   def map(modifier: (Point, Option[A]) => Option[A]): SparseGrid[A] =
     new SparseGrid[A](
       size,
@@ -173,7 +176,7 @@ final class SparseGrid[A](
       }
     )
 
-  /** */
+  /** Map over and modify all the points on the grid that are within the rectangle */
   def mapRectangle(region: Rectangle)(
       modifier: (Point, Option[A]) => Option[A]
   ): SparseGrid[A] =
@@ -186,11 +189,11 @@ final class SparseGrid[A](
       }
     )
 
-  /** */
+  /** Fill a rectangle on the grid with a value */
   def fillRectangle(region: Rectangle, value: A): SparseGrid[A] =
     mapRectangle(region)((_, _) => Option(value))
 
-  /** */
+  /** Map over and modify all the points on the grid that are within the circle */
   def mapCircle(circle: Circle)(modifier: (Point, Option[A]) => Option[A]): SparseGrid[A] =
     new SparseGrid[A](
       size,
@@ -201,11 +204,11 @@ final class SparseGrid[A](
       }
     )
 
-  /** */
+  /** Fill a circle on the grid with a value */
   def fillCircle(circle: Circle, value: A): SparseGrid[A] =
     mapCircle(circle)((_, _) => Option(value))
 
-  /** */
+  /** Map over the grid squares on a line (bresenham) and modify */
   @SuppressWarnings(Array("scalafix:DisableSyntax.while", "scalafix:DisableSyntax.var"))
   def mapLine(from: Point, to: Point)(
       modifier: (Point, Option[A]) => Option[A]
@@ -220,17 +223,17 @@ final class SparseGrid[A](
       }
     )
 
-  /** */
+  /** Map over the grid squares on a line (bresenham) and modify */
   def mapLine(line: LineSegment)(modifier: (Point, Option[A]) => Option[A]): SparseGrid[A] =
     mapLine(line.start.toPoint, line.end.toPoint)(modifier)
 
-  /** */
-  def fillLine(line: LineSegment, value: A): SparseGrid[A] =
-    mapLine(line.start.toPoint, line.end.toPoint)((_, _) => Option(value))
-
-  /** */
+  /** Fill a line (bresenham) with a value */
   def fillLine(from: Point, to: Point, value: A): SparseGrid[A] =
     mapLine(from, to)((_, _) => Option(value))
+
+  /** Fill a line (bresenham) with a value */
+  def fillLine(line: LineSegment, value: A): SparseGrid[A] =
+    mapLine(line.start.toPoint, line.end.toPoint)((_, _) => Option(value))
 
 object SparseGrid:
 

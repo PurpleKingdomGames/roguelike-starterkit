3276f928eff9463d3278c47976f453ed37942c74
Dave Smith <davesmith.00000@gmail.com>
2024-03-06 22:52:11 +0000

    Fixed #57: Resize / reposition windows from events

--- roguelike-starterkit/src/main/scala/roguelikestarterkit/ui/window/WindowManagerEvent.scala
+++ roguelike-starterkit/src/main/scala/roguelikestarterkit/ui/window/WindowManagerEvent.scala
@@ -1,8 +1,13 @@
 package roguelikestarterkit.ui.window
 
 import indigo.*
+import roguelikestarterkit.ui.datatypes.Bounds
 import roguelikestarterkit.ui.datatypes.Coords
+import roguelikestarterkit.ui.datatypes.Dimensions
 
 enum WindowManagerEvent extends GlobalEvent:
   case Close(id: WindowId)
   case GiveFocusAt(coords: Coords)
+  case Move(id: WindowId, position: Coords)
+  case Resize(id: WindowId, dimensions: Dimensions)
+  case Transform(id: WindowId, bounds: Bounds)

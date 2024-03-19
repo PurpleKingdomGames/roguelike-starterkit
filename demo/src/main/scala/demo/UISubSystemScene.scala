b63660d7f243173a15ca42f98faa452fd135e756
Dave Smith <davesmith.00000@gmail.com>
2024-03-19 22:38:20 +0000

    Create a demo

---
+++ demo/src/main/scala/demo/UISubSystemScene.scala
@@ -0,0 +1,71 @@
+package demo
+
+import indigo.*
+import indigo.scenes.*
+import roguelikestarterkit.*
+
+object UISubSystemScene extends Scene[Size, Model, ViewModel]:
+
+  type SceneModel     = Model
+  type SceneViewModel = ViewModel
+
+  val name: SceneName =
+    SceneName("UI subsystem scene")
+
+  val modelLens: Lens[Model, Model] =
+    Lens.keepLatest
+
+  val viewModelLens: Lens[ViewModel, ViewModel] =
+    Lens.keepLatest
+
+  val eventFilters: EventFilters =
+    EventFilters.Permissive
+
+  val subSystems: Set[SubSystem] =
+    Set(
+      WindowManager(
+        SubSystemId("window manager"),
+        3, // TODO: How to update? Event?
+        charSheet = Model.defaultCharSheet
+      )
+        .register(
+          ColourWindow.window(
+            Model.defaultCharSheet
+          )
+        )
+    )
+
+  def updateModel(
+      context: SceneContext[Size],
+      model: Model
+  ): GlobalEvent => Outcome[Model] =
+    case KeyboardEvent.KeyUp(Key.SPACE) =>
+      Outcome(model).addGlobalEvents(SceneEvent.JumpTo(RogueTerminalEmulatorScene.name))
+
+    case KeyboardEvent.KeyUp(Key.KEY_O) =>
+      Outcome(model).addGlobalEvents(WindowManagerEvent.OpenAt(ColourWindow.windowId, Coords(1, 1)))
+
+    case WindowEvent.MouseOver(id) =>
+      println("Mouse over window: " + id)
+      Outcome(model)
+
+    case WindowEvent.MouseOut(id) =>
+      println("Mouse out window: " + id)
+      Outcome(model)
+
+    case _ =>
+      Outcome(model)
+
+  def updateViewModel(
+      context: SceneContext[Size],
+      model: Model,
+      viewModel: ViewModel
+  ): GlobalEvent => Outcome[ViewModel] =
+    _ => Outcome(viewModel)
+
+  def present(
+      context: SceneContext[Size],
+      model: Model,
+      viewModel: ViewModel
+  ): Outcome[SceneUpdateFragment] =
+    Outcome(SceneUpdateFragment.empty)
